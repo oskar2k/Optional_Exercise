1e5fc85ac3ba54de083436cddf8be06882263d9a
oskar2k <70531955+oskar2k@users.noreply.github.com>
2020-08-31 18:19:04 +0200

    Add files via upload
    First check-in

---
+++ calculator.py
@@ -0,0 +1,4 @@
+from decimal import Decimal
+
+def add(x, y):
+    return x + y
---
+++ test_calculator.py
@@ -0,0 +1,6 @@
+import calculator
+import math
+import pytest
+
+def test_add_exercise_1():
+    assert calculator.add(1, 2) == 3
